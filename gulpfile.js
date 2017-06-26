347c88dde901733accdb4d3ed84776725635dd56
Alen Vuletic <alen@slicejack.com>
2017-06-26 14:31:08 +0200

    Gulp updated to v4. gulpfile.js updated.

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,122 @@
-( function() {
+var gulp = require( 'gulp' ),
+		sass = require( 'gulp-sass' ),
+		gutil = require( 'gulp-util' ),
+		inject = require( 'gulp-inline-source' ),
+		inlinecss = require( 'gulp-inline-css' ),
+		del = require( 'del' ),
+		stripComments = require( 'gulp-strip-comments' ),
+		connect = require( 'gulp-connect' ),
+		filesToWatch = [
+			'source/sass/**/*.scss',
+			'source/*.html',
+		];
+
+// Build SASS
+gulp.task( 'build:sass', function() {
 	'use strict';
 
-	// Variables init
-	var gulp = require( 'gulp' ),
-	    util = require( 'gulp-util' ),
-	    sass = require( 'gulp-sass' ),
-	    connect = require( 'gulp-connect' ),
-	    inlinecss = require( 'gulp-inline-css' ),
-	    inject = require( 'gulp-inline-source' ),
-	    stripComments = require( 'gulp-strip-comments' ),
-	    filesToWatch = [
-	    	'source/sass/**/*.scss',
-	    	'source/*.html',
-	    ];
+	return gulp.src( 'source/sass/style.scss' )
+		.pipe(
+			sass( { outputStyle: 'compressed' } )
+			.on( 'error', gutil.log )
+		)
+		.pipe( gulp.dest( 'public/css/' ) );
+} );
 
-	// Style to CSS
-	gulp.task( 'styleToCss', function() {
-		return gulp.src( 'source/sass/style.scss' )
-			.pipe(
-				sass( { outputStyle: 'compressed' } )
-				.on( 'error', util.log )
-			)
-			.pipe( gulp.dest( 'public/css/' ) );
-	} );
+// Inject CSS
+gulp.task( 'inject:css', function() {
+	'use strict';
 
-	// Inject CSS to head
-	gulp.task( 'injectCss', [ 'styleToCss' ], function() {
-		return gulp.src( 'source/*.html' )
-			.pipe( inject() )
-			.pipe( gulp.dest( 'public/' ) );
-	} );
+	return gulp.src( 'source/*.html' )
+		.pipe(
+			inject()
+			.on( 'error', gutil.log )
+		)
+		.pipe( gulp.dest( 'public/' ) );
+} );
 
-	// Inline CSS
-	gulp.task( 'inlinecss', [ 'injectCss' ], function() {
-		return gulp.src( 'public/*.html' )
-			.pipe( inlinecss( {
+// Inline CSS
+gulp.task( 'inline:css', function() {
+	'use strict';
+
+	return gulp.src( 'public/*.html' )
+		.pipe(
+			inlinecss( {
 				applyLinkTags: false,
-				preserveMediaQueries: true
-			} ) )
-			.pipe( gulp.dest( 'public/' ) );
+				preserveMediaQueries: true,
+			} )
+			.on( 'error', gutil.log )
+		)
+		.pipe( gulp.dest( 'public/' ) );
+} );
+
+// Clean CSS
+gulp.task( 'clean:css', function() {
+	'use strict';
+
+	return del( [ 'public/css/' ] );
+} );
+
+// Clean HTML
+gulp.task( 'clean:html', function() {
+	'use strict';
+
+	return gulp.src( 'public/*.html' )
+		.pipe(
+			stripComments( { safe: true } )
+			.on( 'error', gutil.log )
+		)
+		.pipe( gulp.dest( 'public/' ) )
+		.pipe( connect.reload() );
+} );
+
+// Server w/ live reload
+gulp.task( 'connect', function( done ) {
+	'use strict';
+
+	connect.server( {
+		port: 8000,
+		root: 'public',
+		livereload: true,
 	} );
 
-	// Strip comments
-	gulp.task( 'removeComments', [ 'inlinecss' ], function() {
-		return gulp.src( 'public/*.html' )
-			.pipe( stripComments( {
-				safe: true
-			} ) )
-			.pipe( gulp.dest( 'public/' ) )
-			.pipe( connect.reload() );
-	} );
+	done();
+} );
 
-	// Server w/ live reload
-	gulp.task( 'connect', function() {
-		connect.server( {
-			port: 8000,
-			root: 'public',
-			livereload: true,
-		} );
-	} );
+// Build
+gulp.task(
+	'build',
+	gulp.series( [
+		'build:sass',
+		'inject:css',
+		'inline:css',
+		'clean:css',
+		'clean:html',
+	] )
+);
 
-	// Watch
-	gulp.task( 'watch', function() {
-		gulp.watch( filesToWatch, [ 'removeComments' ] );
-	} );
+// Watch
+gulp.task( 'watch', function( done ) {
+	'use strict';
 
-	// Default
-	gulp.task( 'default', [ 'connect', 'removeComments', 'watch' ] );
-}() );
+	gulp.watch( filesToWatch, gulp.series( [ 'build' ] ) );
+
+	done();
+} );
+
+// Default task
+gulp.task(
+	'default',
+	gulp.parallel( [
+		'connect',
+		'build',
+		'watch',
+	] )
+);
+
+// Clean project folder
+gulp.task( 'clean', function() {
+	'use strict';
+
+	return del( [ 'public/' ] );
+} );
