d52cee62a5ef8b6a9738ea06f3d7a21dc5a5db3f
Alen Vuletic <alen@slicejack.com>
2017-06-26 09:20:55 +0200

    Added ESlinter config file. Updated gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,70 @@
-var gulp = require( 'gulp' ),
-    util = require( 'gulp-util' ),
-    sass = require( 'gulp-sass' ),
-    connect = require( 'gulp-connect' ),
-    inlinecss = require( 'gulp-inline-css' ),
-    inject = require( 'gulp-inline-source' ),
-    stripComments = require( 'gulp-strip-comments' ),
-    filesToWatch = [
-        'source/sass/**/*.scss',
-        'source/*.html',
-    ];
+( function() {
+	'use strict';
 
-// Style to CSS
-gulp.task( 'styleToCss', function() {
-    return gulp.src( 'source/sass/style.scss' )
-        .pipe(
-            sass( { outputStyle: 'compressed' } )
-            .on( 'error', util.log )
-        )
-        .pipe( gulp.dest( 'public/css/' ) )
-} );
+	// Variables init
+	var gulp = require( 'gulp' ),
+	    util = require( 'gulp-util' ),
+	    sass = require( 'gulp-sass' ),
+	    connect = require( 'gulp-connect' ),
+	    inlinecss = require( 'gulp-inline-css' ),
+	    inject = require( 'gulp-inline-source' ),
+	    stripComments = require( 'gulp-strip-comments' ),
+	    filesToWatch = [
+	    	'source/sass/**/*.scss',
+	    	'source/*.html',
+	    ];
 
-// Inject CSS to head
-gulp.task( 'injectCss', [ 'styleToCss' ], function () {
-    return gulp.src( 'source/*.html' )
-        .pipe( inject() )
-        .pipe( gulp.dest( 'public/' ) );
-} );
+	// Style to CSS
+	gulp.task( 'styleToCss', function() {
+		return gulp.src( 'source/sass/style.scss' )
+			.pipe(
+				sass( { outputStyle: 'compressed' } )
+				.on( 'error', util.log )
+			)
+			.pipe( gulp.dest( 'public/css/' ) );
+	} );
 
-// Inline CSS
-gulp.task( 'inlinecss', [ 'injectCss' ], function() {
-    return gulp.src( 'public/*.html' )
-        .pipe( inlinecss( {
-            applyLinkTags: false,
-            preserveMediaQueries: true
-        } ) )
-        .pipe( gulp.dest( 'public/' ) );
-} );
+	// Inject CSS to head
+	gulp.task( 'injectCss', [ 'styleToCss' ], function() {
+		return gulp.src( 'source/*.html' )
+			.pipe( inject() )
+			.pipe( gulp.dest( 'public/' ) );
+	} );
 
-// Strip comments
-gulp.task( 'removeComments', [ 'inlinecss' ], function () {
-  return gulp.src( 'public/*.html' )
-    .pipe( stripComments({
-        safe: true
-    }) )
-    .pipe( gulp.dest( 'public/' ) )
-    .pipe( connect.reload() );
-} );
+	// Inline CSS
+	gulp.task( 'inlinecss', [ 'injectCss' ], function() {
+		return gulp.src( 'public/*.html' )
+			.pipe( inlinecss( {
+				applyLinkTags: false,
+				preserveMediaQueries: true
+			} ) )
+			.pipe( gulp.dest( 'public/' ) );
+	} );
 
-// Server w/ live reload
-gulp.task( 'connect', function() {
-    connect.server( {
-        port: 8000,
-        root: 'public',
-        livereload: true,
-    } );
-} );
+	// Strip comments
+	gulp.task( 'removeComments', [ 'inlinecss' ], function() {
+		return gulp.src( 'public/*.html' )
+			.pipe( stripComments( {
+				safe: true
+			} ) )
+			.pipe( gulp.dest( 'public/' ) )
+			.pipe( connect.reload() );
+	} );
 
-// Watch
-gulp.task( 'watch', function() {
-    gulp.watch( filesToWatch, [ 'removeComments' ] );
-} );
+	// Server w/ live reload
+	gulp.task( 'connect', function() {
+		connect.server( {
+			port: 8000,
+			root: 'public',
+			livereload: true,
+		} );
+	} );
 
-// Default
-gulp.task( 'default', [ 'connect', 'removeComments', 'watch' ] );
+	// Watch
+	gulp.task( 'watch', function() {
+		gulp.watch( filesToWatch, [ 'removeComments' ] );
+	} );
+
+	// Default
+	gulp.task( 'default', [ 'connect', 'removeComments', 'watch' ] );
+}() );
